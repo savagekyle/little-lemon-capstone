8046245100cb2b9edca9c18ca0a1247e20c6631d
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-07-26 13:15:20 -0400

    initial booking page setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import food from "../assets/images/restauranfood.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -12,7 +13,9 @@ const Header = () => {
             We are a familiy owned Mediterranean restaurant, focused on
             traditional recipes served with a modern twist.
           </p>
-          <button>Reserve a Table</button>
+          <Link to="/booking">
+            <button>Reserve a Table</button>
+          </Link>
         </article>
         <div className="img-wrapper">
           <img className="header-img" src={food} alt="" />
--- src/scenes/BookingPage.js
+++ src/scenes/BookingPage.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BookingPage = () => {
-  return <div>BookingPage</div>;
+  // const [date, setDate] = useState("");
+  // const [time, setTime] = useState("");
+  // const [guests, setGuests] = useState(1);
+  // const [occasion, setOccasion] = useState("");
+
+  return (
+    <div className="container">
+      <form>
+        <label for="res-date">Choose date</label>
+        <input type="date" id="res-date" />
+        <label for="res-time">Choose time</label>
+        <select id="res-time ">
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+        </select>
+        <label for="guests">Number of guests</label>
+        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <label for="occasion">Occasion</label>
+        <select id="occasion">
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <button type="submit">Make Your Reservation</button>
+      </form>
+    </div>
+  );
 };
 
 export default BookingPage;

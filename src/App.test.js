e94ef03125320186826a0b7fbe5be77ac5d409d2
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-07-28 11:57:04 -0400

    set up unit tests for booking form component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import BookingForm from "./components/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const availableTimesMock = [
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+  "23:00",
+];
+
+test("Renders the BookingForm heading", () => {
+  render(<BookingForm availableTimes={availableTimesMock} />);
+  const headingElement = screen.getByText("Create a Reservation");
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("updateTimes is called correctly when date form field is changed", () => {
+  const mockUpdateTimes = jest.fn();
+
+  render(
+    <BookingForm
+      availableTimes={availableTimesMock}
+      updateTimes={mockUpdateTimes}
+    />
+  );
+
+  const dateInput = screen.getByLabelText("Choose date");
+  fireEvent.change(dateInput, { target: { value: "2023-07-30" } });
+
+  expect(mockUpdateTimes).toHaveBeenCalledTimes(1);
+  expect(mockUpdateTimes).toHaveBeenCalledWith(expect.any(Object));
 });
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const BookingForm = (props) => {
   const [date, setDate] = useState("");

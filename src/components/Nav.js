19086953a2b3a69fba6e3402fd7de79581514717
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-07-31 12:59:42 -0400

    fix overall mobile responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/images/Logo .svg";
 import { Link } from "react-router-dom";
+import Hamburger from "../assets/images/🦆 icon _hamburger menu.svg";
 
 const Nav = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <nav className="container">
       <Link to="/">
         <img src={Logo} alt="Little Lemon Logo" />
       </Link>
 
-      <ul>
+      <div
+        className={`menu-toggle ${isMobileMenuOpen ? "open" : ""}`}
+        onClick={toggleMobileMenu}
+      >
+        <img src={Hamburger} alt="Hamburger Menu" />
+      </div>
+
+      <ul className={`menu-items ${isMobileMenuOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMobileMenu}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMobileMenu}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/menu">Menu</Link>
+          <Link to="/menu" onClick={closeMobileMenu}>
+            Menu
+          </Link>
         </li>
         <li>
-          <Link to="/booking">Reservations</Link>
+          <Link to="/booking" onClick={closeMobileMenu}>
+            Reservations
+          </Link>
         </li>
         <li>
-          <Link to="/order-online">Order Online</Link>
+          <Link to="/order-online" onClick={closeMobileMenu}>
+            Order Online
+          </Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to="/login" onClick={closeMobileMenu}>
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
